2f61572142a020b6335f9c510a8544c483a8d8a1
089245 <sunyongjun@sf-express.com>
2018-09-06 18:01:07 +0800

    remove persistence* auto configuration

--- src/main/java/com/sf/ground/lcn/Main.java
+++ src/main/java/com/sf/ground/lcn/Main.java
@@ -1,6 +1,7 @@
 package com.sf.ground.lcn;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.dao.PersistenceExceptionTranslationAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration;
@@ -12,7 +13,7 @@ import org.springframework.context.annotation.ImportResource;
 @ImportResource({"classpath*:META-INF/spring/*"})
 @EnableDiscoveryClient
 @SpringBootApplication(exclude = {RedisAutoConfiguration.class, DataSourceAutoConfiguration.class,
-        TransactionAutoConfiguration.class})
+        TransactionAutoConfiguration.class, PersistenceExceptionTranslationAutoConfiguration.class})
 @ComponentScan({"com.codingapi.tx", "com.sf.ground.lcn.extend"})
 public class Main {
     public static void main(String[] args) {
